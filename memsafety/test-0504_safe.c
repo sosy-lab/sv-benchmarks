6b287337ebafa52bd3299b6eb1de16ed8984e8a0
Dirk Beyer <Dirk.Beyer@sosy-lab.org>
2012-07-29 13:07:11 +0000

    Submission by Kamil, was submitted on time, but not yet processed
    git-svn-id: https://svn.sosy-lab.org/software/sv-benchmarks/trunk@103 770dfece-24ca-4306-9060-4abab334d2a5

---
+++ memsafety/test-0504_safe.c
@@ -0,0 +1,78 @@
+#include <stdlib.h>
+
+extern int __VERIFIER_nondet_int(void);
+
+int main() {
+
+    struct T {
+        struct T* next;
+        struct T* prev;
+        int data;
+    };
+
+    struct T2 {
+        struct T head;
+        struct T2* next;
+        struct T2* prev;
+    };
+
+    struct T2* first = NULL;
+    struct T2* last = NULL;
+
+    while (__VERIFIER_nondet_int()) {
+
+        struct T2* x = malloc(sizeof(struct T2));
+        if (!x)
+            abort();
+
+        x->next = NULL;
+        x->prev = NULL;
+        x->head.next = &x->head;
+        x->head.prev = &x->head;
+        x->head.data = 0;
+
+        struct T* y = NULL;
+
+        while (__VERIFIER_nondet_int()) {
+            y = malloc(sizeof(struct T));
+            if (!y)
+                abort();
+
+            y->next = x->head.next;
+            y->next->prev = y;
+            y->prev = &x->head;
+            y->data = 0;
+            x->head.next = y;
+            y = NULL;
+        }
+
+        if (!first) {
+            first = x;
+            last = x;
+        } else {
+            last->next = x;
+            x->prev = last;
+            last = x;
+        }
+
+    }
+
+    while (first) {
+
+        struct T2* x = first;
+        first = first->next;
+
+        struct T* y = x->head.next;
+        while (y != &x->head) {
+            struct T* z = y;
+            y = y->next;
+            free(z);
+        }
+
+        free(x);
+
+    }
+
+    return 0;
+
+}
