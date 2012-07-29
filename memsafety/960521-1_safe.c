ff3803edad77a58d3a07dc99e2e2b103f89366c4
Dirk Beyer <Dirk.Beyer@sosy-lab.org>
2012-07-29 13:05:41 +0000

    Submission by Petr, was submitted on time, but not yet processed
    git-svn-id: https://svn.sosy-lab.org/software/sv-benchmarks/trunk@102 770dfece-24ca-4306-9060-4abab334d2a5

---
+++ memsafety/960521-1_safe.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+
+int *a, *b;
+int n;
+
+#define BLOCK_SIZE 32768
+
+void foo ()
+{
+  int i;
+  for (i = 0; i < n; i++)
+    a[i] = -1;
+  for (i = 0; i < BLOCK_SIZE - 1; i++)
+    b[i] = -1;
+}
+
+int main ()
+{
+  n = BLOCK_SIZE;
+  a = malloc (n * sizeof(*a));
+  b = malloc (n * sizeof(*b));
+  *b++ = 0;
+  foo ();
+  if (b[-1])
+  { free(a); free(b); } /* invalid, but branch not accessible */
+  else
+  { free(a); free(b-1); }
+  return 0;
+}
