db6bebee547c23582fae95b828c964937dc65a2d
Dirk Beyer <Dirk.Beyer@sosy-lab.org>
2012-08-09 12:18:52 +0000

    Fixed some typos (in reserved names)
    git-svn-id: https://svn.sosy-lab.org/software/sv-benchmarks/trunk@117 770dfece-24ca-4306-9060-4abab334d2a5

--- loops/trex03_unsafe.c
+++ loops/trex03_unsafe.c
@@ -1,9 +1,9 @@
-unsigned int __VERIFIER_uint();
+unsigned int __VERIFIER_nondet_uint();
 _Bool __VERIFIER_bool();
 
 int main()
 {
-  unsigned int x1=__VERIFIER_uint(), x2=__VERIFIER_uint(), x3=__VERIFIER_uint();
+  unsigned int x1=__VERIFIER_nondet_uint(), x2=__VERIFIER_nondet_uint(), x3=__VERIFIER_nondet_uint();
   unsigned int d1=1, d2=1, d3=1;
   _Bool c1=__VERIFIER_bool(), c2=__VERIFIER_bool();
   
