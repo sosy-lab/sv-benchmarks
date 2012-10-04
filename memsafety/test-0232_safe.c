93815995929c3babf8caf55e4504ce1dfde3f222
Dirk Beyer <Dirk.Beyer@sosy-lab.org>
2012-10-04 15:43:07 +0000

    Revert "Rename value for verification result (notational)", which accidentally happened to delete files.
    This reverts commit 8175aadfcae38c8ee21e369046a921826658c702.
    
    git-svn-id: https://svn.sosy-lab.org/software/sv-benchmarks/trunk@157 770dfece-24ca-4306-9060-4abab334d2a5

---
+++ memsafety/test-0232_safe.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+
+extern int __VERIFIER_nondet_int(void);
+
+struct item {
+    struct item *next;
+    struct item *data;
+};
+
+static void append(struct item **plist)
+{
+    struct item *item = malloc(sizeof *item);
+    item->next = *plist;
+
+    // shared data
+    item->data = (item->next)
+        ? item->next->data
+        : malloc(sizeof *item);
+
+    *plist = item;
+}
+
+int main()
+{
+    struct item *list = NULL;
+
+    // create a long singly-linked list
+    do
+        append(&list);
+    while (__VERIFIER_nondet_int());
+
+    // remove the frist item
+    if (list) {
+        struct item *next = list->next;
+
+        // free shared data
+        free(list->data);
+
+        free(list);
+        list = next;
+    }
+
+    // shared data is already freed when entering the loop
+
+    while (list) {
+        struct item *next = list->next;
+        free(list);
+        list = next;
+    }
+
+    return 0;
+}
