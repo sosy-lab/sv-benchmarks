5190fa1d439f23c94ee5c03047e58c780f60c7ad
Dirk Beyer <Dirk.Beyer@sosy-lab.org>
2013-08-18 16:17:40 +0000

    Contributed by Alexander Schremmer. Add new benchmarks that exercise heap semantics of the tools. The list-properties/*.c files are similar to their safe counterparts - I usually just swapped constants. The heap-manipulation/tree* files are new benchmarks that build a simple tree and check certain data properties about it.
    git-svn-id: https://svn.sosy-lab.org/software/sv-benchmarks/trunk@247 770dfece-24ca-4306-9060-4abab334d2a5

---
+++ c/heap-manipulation/tree_safe.c
@@ -0,0 +1,105 @@
+#include <stdlib.h>
+
+/* Builds a tree with parent links and
+ * checks whether the values are still correct.
+ */
+
+//extern int __VERIFIER_nondet_int(void);
+
+int __VERIFIER_nondet_int(void) {
+    return rand() / 1000;
+}
+
+
+
+static void fail(void) {
+ERROR:
+    goto ERROR;
+}
+
+#define ___MY_ASSERT(cond) do {     \
+    if (!(cond))                    \
+        fail();                     \
+} while (0)
+
+struct node {
+    struct node     *left;
+    struct node     *right;
+    struct node     *parent;
+    int             value;
+};
+
+static void inspect(struct node *node)
+{
+    ___MY_ASSERT(node);
+    while (node != NULL) {
+        if (node->left)
+            ___MY_ASSERT(node->left->value == 42);
+        ___MY_ASSERT(node->value);
+        node = node->parent;
+    }
+}
+
+struct node *create_tree()
+{
+    struct node *nodelast = NULL;
+    struct node *node = NULL;
+    while (__VERIFIER_nondet_int()) {
+        node = malloc(sizeof *node);
+        if (!node)
+            abort();
+
+        node->left = NULL;
+        node->right = nodelast;
+        if (nodelast)
+            nodelast->parent = node;
+        int value = __VERIFIER_nondet_int();
+        if (!value)
+            value = 1;
+        node->value = value;
+        nodelast = node;
+    }
+    while (node != NULL) {
+        node->left = malloc(sizeof *node);
+        if (!node)
+            abort();
+        node->left->left = NULL;
+        node->left->right = NULL;
+        node->left->value = 42;
+        node->left->parent = node;
+        node = node->right;
+    }
+    return nodelast;
+}
+
+void free_tree(struct node *node) {
+    struct node *last = NULL;
+    while (1) {
+        if (node->right == NULL)
+            break;
+        node = node->right;
+    }
+    while (node != NULL) {
+        if (node->left)
+            free(node->left);
+        if (node->right)
+            free(node->right);
+        last = node;
+        node = node->parent;
+    }
+    free(last);
+}
+
+int main()
+{
+    struct node *data = create_tree();
+
+    if (!data)
+        return EXIT_SUCCESS;
+
+    inspect(data);
+
+    free_tree(data);
+
+    return EXIT_SUCCESS;
+}
---
+++ c/list-properties/list_flag_unsafe.c
@@ -0,0 +1,55 @@
+extern int __VERIFIER_nondet_int();
+/*
+ * Variation on example 0: use a (non-deterministic) boolean
+ * flag to set the value of the elements in the list before
+ * 3 to a value depending on the flag, and check later on
+ * that the list is what has been built just before.
+ */
+#include <stdlib.h>
+/*  #include "assert.h" */
+
+void exit(int s) {
+ _EXIT: goto _EXIT;
+}
+
+typedef struct node {
+  int h;
+  struct node *n;
+} *List;
+
+void main() {
+  int flag = __VERIFIER_nondet_int();
+  List p, a, t;
+
+  /* Build a list of the form x->x->x->...->x->3
+   * with x depending on some flag
+   */
+  a = (List) malloc(sizeof(struct node));
+  if (a == 0) exit(1);
+  p = a;
+  while (__VERIFIER_nondet_int()) {
+    if (flag) {
+      p->h = 1;
+    } else {
+      p->h = 2;
+    }
+    /*** TVLA forgets at this point the dependence
+	 between p->h and the value of flag        ***/
+    t = (List) malloc(sizeof(struct node));
+    if (t == 0) exit(1);
+    p->n = t;
+    p = p->n;
+  }
+  p->h = 3;
+    
+  /* Check it */
+  p = a;
+  if (flag)
+    while (p->h == 2)
+      p = p->n;
+  else
+    while (p->h == 1)
+      p = p->n;
+  if (p->h != 3)
+    ERROR: goto ERROR;
+}
