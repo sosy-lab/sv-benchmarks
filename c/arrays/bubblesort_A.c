b7683f5611522f4b18cc0bba062ced6f9ea9cf11
Dirk Beyer <Dirk.Beyer@sosy-lab.org>
2014-01-08 14:57:55 +0000

    array task added
    git-svn-id: https://svn.sosy-lab.org/software/sv-benchmarks/trunk@413 770dfece-24ca-4306-9060-4abab334d2a5

---
+++ c/arrays/bubblesort_A.c
@@ -0,0 +1,40 @@
+
+extern int nondet_int();
+
+int main( void ) { 
+  
+  int N = nondet_int( );
+  int a [N];
+  int i = 0;
+  while ( i < N ) {
+    a[i] = nondet_int( );
+    i++;
+  }
+  
+  int swapped = 1;
+  while ( swapped == 1 ) { 
+    swapped = 0;
+    i = 1;
+    while ( i < N ) { 
+      if ( a[i-1] > a[i] ) { 
+        int t = a[i];
+        a[i] = a[i-1];
+        a[i-1] = t;
+        swapped = 1;
+      }   
+      i = i + 1;
+    }   
+  }
+  
+  int x, y;
+  for ( x = 0 ; x < N ; x++ ) {
+    for ( y = x+1 ; y < N ; y++ ) {
+      if ( a[x] > a[y] ) {
+        ERROR: goto ERROR;
+      }
+    } 
+  }
+  
+  return 0;
+}
+
