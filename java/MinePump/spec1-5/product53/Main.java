e9911864b44bb2d23da9b64954af69b1794d4430
Philipp Wendler <uni@philippwendler.de>
2015-09-14 14:58:42 +0200

    Add declaration of undeclared variable.

--- java/MinePump/spec1-5/product53/Main.java
+++ java/MinePump/spec1-5/product53/Main.java
@@ -27,6 +27,7 @@ public  class  Main {
 			boolean action1 = getBoolean();
 			boolean action2 = getBoolean();
 			boolean action3 = getBoolean();			
+			boolean action4 = false;
 			if (!action3) action4 = getBoolean();
 
 			if (action1) {
