464939abd31d9d8cf75c3c53e000f9823315c803
Dan Liew <daniel.liew@imperial.ac.uk>
2015-10-11 14:50:14 +0100

    Remove some pointlessly preprocessed files and fix up some uses of ``#define bool int`` rather than ``_Bool``.

--- c/loops/trex04_true-unreach-call_false-termination.c
+++ c/loops/trex04_true-unreach-call_false-termination.c
@@ -6,13 +6,12 @@ void __VERIFIER_assert(int cond) {
   }
   return;
 }
-#define bool int
-extern bool __VERIFIER_nondet_bool();
+extern _Bool __VERIFIER_nondet_bool();
 
 void foo()
 {
   int y=0;
-  bool c1=__VERIFIER_nondet_bool(), c2=__VERIFIER_nondet_bool();
+  _Bool c1=__VERIFIER_nondet_bool(), c2=__VERIFIER_nondet_bool();
   if (c1) y++;
   if (c2) y--;
   else y+=10;
@@ -22,7 +21,7 @@ int main()
 {
   int d = 1;
   int x;
-  bool c1=__VERIFIER_nondet_bool(), c2=__VERIFIER_nondet_bool();
+  _Bool c1=__VERIFIER_nondet_bool(), c2=__VERIFIER_nondet_bool();
 
   if (c1) d = d - 1;
   if (c2) foo();
