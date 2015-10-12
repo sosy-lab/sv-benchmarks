446622da0098f3b5a6078551781ef8f880ef0e2b
Tsung-Ju Lii <usefulalgorithm@gmail.com>
2015-10-12 17:10:38 +0800

    Changed the filenames of original file
    Changed the filenames of the incorrect files from true-unreach to
    false-unreach.

---
+++ c/array-examples/sorting_selectionsort_false-unreach-call_ground.c
@@ -0,0 +1,44 @@
+extern void __VERIFIER_error() __attribute__ ((__noreturn__));
+void __VERIFIER_assert(int cond) { if(!(cond)) { ERROR: __VERIFIER_error(); } }
+
+#define N 100000
+
+int main( ) {
+  int a[ N ];
+  int i = 0;
+  int x;
+  int y;
+  while ( i < N ) {
+    int k = i + 1;
+    int s = i;
+    while ( k < N ) {
+      if ( a[k] < a[s] ) {
+        s = k;
+      }
+      k = k+1;
+    }
+    if ( s != i ) {
+      int tmp = a[s];
+      a[s] = a[i];
+      a[i] = tmp;
+    }
+    
+    for ( x = 0 ; x < i ; x++ ) {
+      for ( y = x + 1 ; y < i ; y++ ) {
+        __VERIFIER_assert(  a[x] <= a[y]  );
+      }
+    }
+    for ( x = 0 ; x < N ; x++ ) {
+      __VERIFIER_assert(  a[x] >= a[i]  );
+    }
+    
+    i = i+1;
+  }
+    
+  for ( x = 0 ; x < N ; x++ ) {
+    for ( y = x + 1 ; y < N ; y++ ) {
+      __VERIFIER_assert(  a[x] <= a[y]  );
+    }
+  }
+  return 0;
+}
---
+++ c/array-examples/sorting_selectionsort_false-unreach-call_ground.i
@@ -0,0 +1,38 @@
+extern void __VERIFIER_error() __attribute__ ((__noreturn__));
+void __VERIFIER_assert(int cond) { if(!(cond)) { ERROR: __VERIFIER_error(); } }
+int main( ) {
+  int a[ 100000 ];
+  int i = 0;
+  int x;
+  int y;
+  while ( i < 100000 ) {
+    int k = i + 1;
+    int s = i;
+    while ( k < 100000 ) {
+      if ( a[k] < a[s] ) {
+        s = k;
+      }
+      k = k+1;
+    }
+    if ( s != i ) {
+      int tmp = a[s];
+      a[s] = a[i];
+      a[i] = tmp;
+    }
+    for ( x = 0 ; x < i ; x++ ) {
+      for ( y = x + 1 ; y < i ; y++ ) {
+        __VERIFIER_assert( a[x] <= a[y] );
+      }
+    }
+    for ( x = 0 ; x < 100000 ; x++ ) {
+      __VERIFIER_assert( a[x] >= a[i] );
+    }
+    i = i+1;
+  }
+  for ( x = 0 ; x < 100000 ; x++ ) {
+    for ( y = x + 1 ; y < 100000 ; y++ ) {
+      __VERIFIER_assert( a[x] <= a[y] );
+    }
+  }
+  return 0;
+}
