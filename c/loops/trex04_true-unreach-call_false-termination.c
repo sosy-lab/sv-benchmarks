sosy-lab/sv-benchmarks

commit bcc88712864904c35e864d9b3bd32da4abbed168

Fixing more "Loops" benchmarks with UB

c/loops/trex04_true-unreach-call_false-termination.c
@@ -1,4 +1,5 @@
 extern void __VERIFIER_error() __attribute__ ((__noreturn__));
+extern int __VERIFIER_assume(int cond);
 
 void __VERIFIER_assert(int cond) {
   if (!(cond)) {
@@ -22,6 +23,7 @@ int main()
 {
   int d = 1;
   int x = __VERIFIER_nondet_int();
+  __VERIFIER_assume(x <= 1000000 && x >= -1000000);
   _Bool c1=__VERIFIER_nondet_bool(), c2=__VERIFIER_nondet_bool();
 
   if (c1) d = d - 1;

c/loops/trex04_true-unreach-call_false-termination.i
@@ -1,4 +1,5 @@
 extern void __VERIFIER_error() __attribute__ ((__noreturn__));
+extern int __VERIFIER_assume(int cond);
 void __VERIFIER_assert(int cond) {
   if (!(cond)) {
     ERROR: __VERIFIER_error();
@@ -19,6 +20,7 @@ int main()
 {
   int d = 1;
   int x = __VERIFIER_nondet_int();
+  __VERIFIER_assume(x <= 1000000 && x >= -1000000);
   _Bool c1=__VERIFIER_nondet_bool(), c2=__VERIFIER_nondet_bool();
   if (c1) d = d - 1;
   if (c2) foo();

c/loop-invgen/SpamAssassin-loop_true-unreach-call.c
@@ -2,50 +2,50 @@
 
 int main()
 {
-  int len;
-  int i;
-  int j;
+    int len;
+    int i;
+    int j;
 
-  int bufsize;
-  bufsize = __VERIFIER_nondet_int();
-  len = __VERIFIER_nondet_int();
-  int limit = bufsize - 4;
+    int bufsize;
+    bufsize = __VERIFIER_nondet_int();
+    if (bufsize < 0) return 0; // avoid overflows for too negative values
+    len = __VERIFIER_nondet_int();
+    int limit = bufsize - 4;
 
-  if (bufsize < 0) return 0; // avoid overflows for too negative values
-  
-  for (i = 0; i < len; ) {
-    for (j = 0; i < len && j < limit; ){     
-      if (i + 1 < len){ 
-	__VERIFIER_assert(i+1<len);
-	__VERIFIER_assert(0<=i);
-	if( __VERIFIER_nondet_int() ) goto ELSE;
-        __VERIFIER_assert(i<len);
-	__VERIFIER_assert(0<=i);
-        __VERIFIER_assert(j<bufsize);
-	__VERIFIER_assert(0<=j);	
 
-        j++;
-        i++;
-        __VERIFIER_assert(i<len);
-	__VERIFIER_assert(0<=i);
-        __VERIFIER_assert(j<bufsize);
-	__VERIFIER_assert(0<=j);
+    for (i = 0; i < len; ) {
+        for (j = 0; i < len && j < limit; ){
+            if (i + 1 < len){ 
+                __VERIFIER_assert(i+1<len);
+                __VERIFIER_assert(0<=i);
+                if( __VERIFIER_nondet_int() ) goto ELSE;
+                __VERIFIER_assert(i<len);
+                __VERIFIER_assert(0<=i);
+                __VERIFIER_assert(j<bufsize);
+                __VERIFIER_assert(0<=j);
 
-        j++;
-        i++;
-        __VERIFIER_assert(j<bufsize);
-	__VERIFIER_assert(0<=j);
-        j++;
-      } else {
+                j++;
+                i++;
+                __VERIFIER_assert(i<len);
+                __VERIFIER_assert(0<=i);
+                __VERIFIER_assert(j<bufsize);
+                __VERIFIER_assert(0<=j);
+
+                j++;
+                i++;
+                __VERIFIER_assert(j<bufsize);
+                __VERIFIER_assert(0<=j);
+                j++;
+            } else {
 ELSE:
-        __VERIFIER_assert(i<len);
-	__VERIFIER_assert(0<=i);
-        __VERIFIER_assert(j<bufsize);
-	__VERIFIER_assert(0<=j);
-        j++;
-        i++;
-      }
+                __VERIFIER_assert(i<len);
+                __VERIFIER_assert(0<=i);
+                __VERIFIER_assert(j<bufsize);
+                __VERIFIER_assert(0<=j);
+                j++;
+                i++;
+            }
+        }
     }
-  }
-  return 0;
+    return 0;
 }

c/loop-invgen/SpamAssassin-loop_true-unreach-call.i
@@ -14,9 +14,9 @@ int main()
   int j;
   int bufsize;
   bufsize = __VERIFIER_nondet_int();
+  if (bufsize < 0) return 0;
   len = __VERIFIER_nondet_int();
   int limit = bufsize - 4;
-  if (bufsize < 0) return 0;
   for (i = 0; i < len; ) {
     for (j = 0; i < len && j < limit; ){
       if (i + 1 < len){

c/loop-lit/cggmp2005_variant_true-unreach-call.c
@@ -8,7 +8,7 @@ int main() {
     int lo, mid, hi;
     lo = 0;
     mid = __VERIFIER_nondet_int();
-    __VERIFIER_assume(mid > 0 && mid <= __VERIFIER_nondet_int());
+    __VERIFIER_assume(mid > 0 && mid <= LARGE_INT);
     hi = 2*mid;
     
     while (mid > 0) {

c/loop-lit/cggmp2005_variant_true-unreach-call.c.i
@@ -11,7 +11,7 @@ int main() {
     int lo, mid, hi;
     lo = 0;
     mid = __VERIFIER_nondet_int();
-    __VERIFIER_assume(mid > 0 && mid <= __VERIFIER_nondet_int());
+    __VERIFIER_assume(mid > 0 && mid <= 1000000);
     hi = 2*mid;
     while (mid > 0) {
         lo = lo + 1;

c/loop-new/half_true-unreach-call.c
@@ -3,11 +3,11 @@ int main() {
     int i = 0;
     int n = 0;
     int k = __VERIFIER_nondet_int();
-    __VERIFIER_assume(k <= LARGE_INT);
+    __VERIFIER_assume(k <= LARGE_INT && k >= -LARGE_INT);
     for(i = 0; i < 2*k; i++) {
-	if (i % 2 == 0) {
-	    n ++;
-	}
+        if (i % 2 == 0) {
+            n ++;
+        }
     }
     __VERIFIER_assert(k < 0 || n == k);
     return 0;

c/loop-new/half_true-unreach-call.i
@@ -11,7 +11,7 @@ int main() {
     int i = 0;
     int n = 0;
     int k = __VERIFIER_nondet_int();
-    __VERIFIER_assume(k <= 1000000);
+    __VERIFIER_assume(k <= 1000000 && k >= -1000000);
     for(i = 0; i < 2*k; i++) {
  if (i % 2 == 0) {
      n ++;

commit f4cfbfbed1d434902a9a640175e2a25ab03d7d0c

Fix incorrect function signature for __VERIFIER_assume() introduced by bcc88712864904c35e864d9b3bd32da4abbed168 which broke the build.

c/loops/trex04_true-unreach-call_false-termination.c
@@ -1,5 +1,5 @@
 extern void __VERIFIER_error() __attribute__ ((__noreturn__));
-extern int __VERIFIER_assume(int cond);
+extern void __VERIFIER_assume(int cond);
 
 void __VERIFIER_assert(int cond) {
   if (!(cond)) {

c/loops/trex04_true-unreach-call_false-termination.i
@@ -1,5 +1,5 @@
 extern void __VERIFIER_error() __attribute__ ((__noreturn__));
-extern int __VERIFIER_assume(int cond);
+extern void __VERIFIER_assume(int cond);
 void __VERIFIER_assert(int cond) {
   if (!(cond)) {
     ERROR: __VERIFIER_error();