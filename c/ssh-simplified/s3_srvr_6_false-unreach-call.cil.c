a73a1e67eae822aa023af3bb7ace0861f8a0b74b
Mitch <mitchellgerrard@gmail.com>
2016-01-26 13:21:40 -0600

    adding ulong extern function declarations

--- c/ssh-simplified/s3_srvr_6_false-unreach-call.cil.c
+++ c/ssh-simplified/s3_srvr_6_false-unreach-call.cil.c
@@ -1,5 +1,4 @@
 extern void __VERIFIER_error() __attribute__ ((__noreturn__));
-extern void __VERIFIER_assume(int);
 
 extern char __VERIFIER_nondet_char(void);
 extern int __VERIFIER_nondet_int(void);
