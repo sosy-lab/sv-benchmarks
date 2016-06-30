d8d690f6e189adfba1e91f1bae4cc5ca1cc65df2
Sebastian Ott <ottseb@fim.uni-passau.de>
2016-06-30 13:04:40 +0200

    Fix termination property
    The changes from cheshire@a934dcc72d1ae14e137ff953a3840494b11aeb72  causes termination of the program.

--- c/loops/sum03_true-unreach-call_false-termination.c
+++ c/loops/sum03_true-unreach-call_false-termination.c
@@ -10,11 +10,11 @@ void __VERIFIER_assert(int cond) {
 unsigned int __VERIFIER_nondet_uint();
 
 int main() { 
-  int sn=0;
+  unsigned int sn=0;
   unsigned int loop1=__VERIFIER_nondet_uint(), n1=__VERIFIER_nondet_uint();
   unsigned int x=0;
 
-  while(x < 1000000){
+  while(1){
     sn = sn + a;
     x++;
     __VERIFIER_assert(sn==x*a || sn == 0);
