4586841e42545925c22f55b820f60954e706798f
leostrakosch <shirmomi@gmail.com>
2016-10-31 16:48:10 +0530

    Fix undefinedness in ControlFlow due to wrong offset, missing declarations

--- c/loop-acceleration/array_false-unreach-call3_true-termination_false-valid-deref.c
+++ c/loop-acceleration/array_false-unreach-call3_true-termination_false-valid-deref.c
@@ -1,23 +1,28 @@
 extern void __VERIFIER_error() __attribute__ ((__noreturn__));
 extern int __VERIFIER_nondet_int(void);
+
 void __VERIFIER_assert(int cond) {
   if (!(cond)) {
     ERROR: __VERIFIER_error();
   }
   return;
 }
+
+#define N 1024
+
 int main(void) {
-  int A[1024];
+  int A[N];
   int i;
 
-  for (i = 0; i < 1024; i++) {
+  for (i = 0; i < N; i++) {
     A[i] = __VERIFIER_nondet_int();
   }
 
   for (i = 0; A[i] != 0; i++) {
-    if (i >= 1024-1) {
+    if (i >= N) {
       break;
     }
   }
-  __VERIFIER_assert(i <= 1024);
+
+  __VERIFIER_assert(i <= N);
 }
