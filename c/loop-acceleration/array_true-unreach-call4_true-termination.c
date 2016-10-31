21ba2f3642ed50145472347725c4c811412f2e6e
leostrakosch <shirmomi@gmail.com>
2016-10-31 16:39:59 +0530

    Fix undefinedness in ControlFlow due to uninitialized arrays

--- c/loop-acceleration/array_true-unreach-call4_true-termination.c
+++ c/loop-acceleration/array_true-unreach-call4_true-termination.c
@@ -1,4 +1,5 @@
 extern void __VERIFIER_error() __attribute__ ((__noreturn__));
+extern int __VERIFIER_nondet_int(void);
 
 void __VERIFIER_assert(int cond) {
   if (!(cond)) {
@@ -12,6 +13,10 @@ void __VERIFIER_assert(int cond) {
 int main(void) {
   int A[N];
   int i;
+
+  for (i = 0; i < N-1; i++) {
+    A[i] = __VERIFIER_nondet_int();
+  }
 
   A[N-1] = 0;
 
