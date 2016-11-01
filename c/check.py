194ab6be14e9cffc4c1edf433076a04c6d560f62
leostrakosch <shirmomi@gmail.com>
2016-11-01 20:37:26 +0530

    Add reducercommutativity/av60_false-valid-deref.c to check.py whitelist

--- c/check.py
+++ c/check.py
@@ -152,6 +152,7 @@ KNOWN_DIRECTORY_PROBLEMS = [
     ("loops", "trex03_true-unreach-call_true-termination_true-termination.c is not contained in any category"),
     ("loops", "heavy_false-unreach-call.c is not contained in any category"),
     ("recursive", "Addition03_false-no-overflow.c is not contained in any category"),
+    ("reducercommutativity", "avg60_false-valid-deref.c is not contained in any category"),
     ("ssh-simplified", "s3_clnt_3.cil_true-unreach-call_true-termination.c is not contained in any category"),
     ("termination-crafted", "LexIndexValue-Pointer_false-valid-deref.c is not contained in any category"),
     ("termination-memory-alloca", "cll_by_lseg-alloca_false-termination.c is not contained in any category"),
