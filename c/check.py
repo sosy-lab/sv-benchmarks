a46723366319208848498debbfcb0e25e954399d
leostrakosch <shirmomi@gmail.com>
2016-11-01 23:52:01 +0530

    Add new false-valid-deref task of loop-acceleration to check.py whitelist

--- c/check.py
+++ c/check.py
@@ -110,6 +110,7 @@ KNOWN_DIRECTORY_PROBLEMS = [
     ("termination-memory-alloca", "LarrazOliverasRodriguez-CarbonellRubio-2013FMCAD-Fig1-alloca_unknown-termination.c has no known verdict"),
     ("termination-memory-alloca", "LarrazOliverasRodriguez-CarbonellRubio-2013FMCAD-Fig1-alloca_unknown-termination.c.i has no known verdict"),
 
+    ("loop-acceleration", "array_false-unreach-call3_true-termination_false-valid-deref.c is not contained in any category"),
     ("termination-memory-alloca", "cll_by_lseg-alloca_false-termination.c is not contained in any category"),
     ("termination-memory-alloca", "cll_by_lseg-alloca_true-termination.c is not contained in any category"),
     ("termination-memory-alloca", "cll_by_lseg_traverse-alloca_false-termination.c is not contained in any category"),
