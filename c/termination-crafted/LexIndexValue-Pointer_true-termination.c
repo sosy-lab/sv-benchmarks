e74ee60e891052fb09ebe66bf1e99dfae99aa8ee
Jera Hensel <jera.hensel@rwth-aachen.de>
2016-11-16 20:00:34 +0100

    resolve warning (include definition for malloc)

--- c/termination-crafted/LexIndexValue-Pointer_true-termination.c
+++ c/termination-crafted/LexIndexValue-Pointer_true-termination.c
@@ -5,6 +5,10 @@
  * 2-lex ranking function: f(p, q, *q) = (p + 1048 - q, *q)
  *
  */
+ typedef long unsigned int size_t;
+
+void * __attribute__((__cdecl__)) malloc (size_t __size) ;
+
 extern int __VERIFIER_nondet_int(void);
 
 int main() {
