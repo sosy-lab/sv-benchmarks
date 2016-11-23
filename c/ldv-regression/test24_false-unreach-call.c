cb6d6b82722be3c74f55b0f42e08014981128e08
Vadim Mutilin <mutilin@ispras.ru>
2016-11-23 15:32:02 +0300

    Add assignment by nondeterministic values

--- c/ldv-regression/test24_false-unreach-call.c
+++ c/ldv-regression/test24_false-unreach-call.c
@@ -2,6 +2,7 @@ extern void __VERIFIER_error() __attribute__ ((__noreturn__));
 
 
 extern void *__VERIFIER_nondet_pointer();
+extern int __VERIFIER_nondet_int();
 
 struct dummy {
   int a, b;
@@ -15,7 +16,12 @@ int check(struct dummy *ad1, int b)
 int main()
 {
   struct dummy ad1[20], *ad2;
-  int i, *pa;
+  int i, j, *pa;
+  for(j=0; j<20; j++) {
+    ad1[j].a = __VERIFIER_nondet_int();
+    ad1[j].b = __VERIFIER_nondet_int();
+  }
+  i = __VERIFIER_nondet_int();
   if (i >= 0 && i < 10) {
     ad2 = ad1;
     ad1[i].a = i;
