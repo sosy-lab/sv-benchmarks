2a51e5d2670b26e325280c64398c920c6c558edd
Philipp Wendler <uni@philippwendler.de>
2016-11-28 21:36:58 +0100

    Mark some issues as known in check.py, category files are still changing.

--- c/check.py
+++ c/check.py
@@ -146,6 +146,9 @@ KNOWN_BENCHMARK_FILE_PROBLEMS = [
     ]
 
 KNOWN_SET_PROBLEMS = [
+    ("ArraysMemSafety.set", "32 bit category contains 64 bit benchmarks in termination-crafted"),
+    ("BitVectorsOverflows.set", "64 bit category contains 32 bit benchmarks in recursive"),
+    ("HeapMemSafety.set", "32 bit category contains 64 bit benchmarks in termination-crafted"),
     ]
 
 KNOWN_GLOBAL_PROBLEMS = [
