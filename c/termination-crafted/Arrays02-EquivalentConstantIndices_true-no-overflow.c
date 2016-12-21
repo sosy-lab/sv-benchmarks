4bb78ede63bc2e27a46d9a085564be010de6bd17
Guoanshisb <polarishehn@gmail.com>
2016-12-21 14:58:04 -0700

    Fix uninitialized array in termination-crafted/Arrays02-EquivalentConstantIndices_true-no-overflow.c

--- c/termination-crafted/Arrays02-EquivalentConstantIndices_true-no-overflow.c
+++ c/termination-crafted/Arrays02-EquivalentConstantIndices_true-no-overflow.c
@@ -7,6 +7,11 @@ extern int __VERIFIER_nondet_int(void);
 
 int main() {
 	int a[1048];
+
+  for (int i = 0; i < 1048; i++) {
+    a[i] = __VERIFIER_nondet_int();
+  }
+
 	while (a[2] >= 0) {
 		a[2] = a[2] - 1;
 		a[1+1] = __VERIFIER_nondet_int();
