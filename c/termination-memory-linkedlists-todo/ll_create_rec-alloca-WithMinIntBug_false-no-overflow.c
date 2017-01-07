7e00edafaccafdbbe586a178b2083e34d7c27b5a
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2017-01-07 11:28:22 +0100

    add orginal files for the fixes in PR #425
    * add original files (from termination-memory-linkedlists) that have overflow
    and memsafety bugs and that were fixed in PR #425
    * I kept only one file for each problem
    * The files are added to a new folder termination-memory-linkedlists-todo
    but we can directly move them to termination-memory-linkedlists after
    the competition

---
+++ c/termination-memory-linkedlists-todo/ll_create_rec-alloca-WithMinIntBug_false-no-overflow.c
@@ -0,0 +1,38 @@
+/*
+ * Date: 30/09/2015
+ * Created by: 
+ *   Ton Chanh Le (chanhle@comp.nus.edu.sg) and
+ *   Duc Muoi Tran (muoitranduc@gmail.com)
+ */
+
+#include <stdlib.h>
+
+
+extern int __VERIFIER_nondet_int();
+
+typedef struct node {
+  int val;
+  struct node* next;
+} node_t;
+
+// Create a new linked list with length n when n >= 0
+// or non-terminating when n < 0 
+node_t* new_ll(int n)
+{
+  if (n == 0)
+    return NULL;
+  node_t* head = malloc(sizeof(node_t));
+  head->val = n;
+  head->next = new_ll(n-1);
+  return head;
+}
+
+int main ()
+{
+  int n = __VERIFIER_nondet_int();
+  node_t* head = new_ll(n);
+  return 0;
+}
+
+
+
