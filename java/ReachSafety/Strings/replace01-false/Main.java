053866f8c1d8bd565cf81adf20b4b619cae2c508
Lucas Cordeiro <lucasccordeiro@gmail.com>
2017-09-13 16:12:34 +0100

    Added the replace benchmarks to the ReachSafety category
    Added four Java benchmarks related to the replace method of the
    String class.

---
+++ java/ReachSafety/Strings/replace01-false/Main.java
@@ -0,0 +1,11 @@
+import java.lang.String;
+import org.svcomp.SVComp;
+
+public class Main {
+    public static void main (String args[]) {
+        String s = "foo"; 
+        boolean cond = SVComp.nondetBoolean();
+        if (cond) 
+            assert (s.replace("oo", "aa").equals("foo"));
+    }
+}
---
+++ java/ReachSafety/Strings/replace02-true/Main.java
@@ -0,0 +1,8 @@
+import java.lang.String;
+
+public class Main {
+    public static void main (String args[]) {
+        String s = "foo"; 
+        assert (s.replace('o', 'a').equals("faa")); 
+    }
+}
