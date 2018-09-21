6ab3eadaeae9487b383aa686330c5f38c5762d1b
avritichauhan <avriti.anna92@gmail.com>
2018-09-21 16:38:22 +0530

    Added benchmarks for ReachSafety Loops.

---
+++ c/loops-crafted-1/Mono1_true-unreach-call1.c
@@ -0,0 +1,15 @@
+extern void __VERIFIER_error() __attribute__ ((__noreturn__));
+void __VERIFIER_assert(int cond) { if(!(cond)) { ERROR: __VERIFIER_error(); } }
+int main(void) {
+  unsigned int x = 0;
+
+  while (x < 100000000) {
+    if (x < 10000000) {
+      x++;
+    } else {
+      x += 2;
+    }
+  }
+
+  __VERIFIER_assert(x == 100000000) ;
+}
