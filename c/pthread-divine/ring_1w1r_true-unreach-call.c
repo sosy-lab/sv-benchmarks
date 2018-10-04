34cbff68c54340962916d72a5ee93cd4efe035a3
Vladimir Still <vl.still@gmail.com>
2018-10-04 14:52:32 +0200

    Put license into a separate file.

--- c/pthread-divine/ring_1w1r_true-unreach-call.c
+++ c/pthread-divine/ring_1w1r_true-unreach-call.c
@@ -1,20 +1,3 @@
-/* This file is distributed under the ISC licence:
-
-Copyright 2018 Vladimír Štill
-
-Permission to use, copy, modify, and/or distribute this software for any
-purpose with or without fee is hereby granted, provided that the above
-copyright notice and this permission notice appear in all copies.
-
-THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
-REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
-FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
-INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
-LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
-OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
-PERFORMANCE OF THIS SOFTWARE.
-*/
-
 /* Contributed by: Vladimír Štill, https://divine.fi.muni.cz
    Description: A test of bounded lock-free queue.
 */
