sosy-lab/sv-benchmarks

commit 55d4083f0e4977ae90dc06a0ce240d372d11103b

Replace memcpy with memmove for overlapping memory
Prior to this commit, some benchmarks called memcpy with overlapping
source and destination areas, which is undefined behaviour according to
POSIX.1-2008 and the C standard. The CBMC tool therefore gives a result
of false, including a counterexample, on the benchmarks that are
expected to return true.

These benchmarks, being examples from device drivers, were supposedly
written with overlapping source and memory regions intentionally, and
the code 'works' in practice. However, SV-COMP participants should
arguably not be expected to get the right result for benchmarks relying
on quirky, non-standards-conformant hardware behaviour.

This commit replaces those memcpy calls with calls to memmove, which has
identical semantics but is defined even when called on overlapping
memory regions. Only those calls that CBMC complained about within a
timeout of 15 minutes were removed; thus, safe calls to memcpy, as well
as overlapping calls that CBMC could not discover, remain in the
benchmarks.

c/ldv-linux-4.2-rc1/linux-4.2-rc1.tar.xz-08_1a-drivers--media--usb--dvb-usb--dvb-usb-dw2102.ko-entry_point_false-unreach-call.cil.out.c
@@ -7727,7 +7727,7 @@ static int dw210x_op_rw(struct usb_device *dev , u8 request , u16 value , u16 in
 
   }
   if (flags == 1) {
-    memcpy((void *)u8buf, (void const   *)data, (size_t )len);
+    memmove((void *)u8buf, (void const   *)data, (size_t )len);
   } else {
 
   }
@@ -10537,7 +10537,7 @@ static int dw2102_load_firmware(struct usb_device *dev , struct firmware  const 
   dw210x_op_rw(dev, 160, 32658, 0, & reset, 1, 1);
   dw210x_op_rw(dev, 160, 58880, 0, & reset, 1, 1);
   if ((unsigned long )p != (unsigned long )((u8 *)0U)) {
-    memcpy((void *)p, (void const   *)fw->data, fw->size);
+    memmove((void *)p, (void const   *)fw->data, fw->size);
     i = 0;
     goto ldv_49014;
     ldv_49013: 

c/ldv-linux-4.2-rc1/linux-4.2-rc1.tar.xz-08_1a-drivers--net--ethernet--intel--i40e--i40e.ko-entry_point_true-unreach-call.cil.out.c
@@ -47968,19 +47968,19 @@ static int i40e_dcbnl_ieee_getets(struct net_device *dev , struct ieee_ets *ets 
   ets->willing = dcbxcfg->etscfg.willing;
   ets->ets_cap = dcbxcfg->etscfg.maxtcs;
   ets->cbs = dcbxcfg->etscfg.cbs;
-  memcpy((void *)(& ets->tc_tx_bw), (void const   *)(& dcbxcfg->etscfg.tcbwtable),
+  memmove((void *)(& ets->tc_tx_bw), (void const   *)(& dcbxcfg->etscfg.tcbwtable),
            8UL);
-  memcpy((void *)(& ets->tc_rx_bw), (void const   *)(& dcbxcfg->etscfg.tcbwtable),
+  memmove((void *)(& ets->tc_rx_bw), (void const   *)(& dcbxcfg->etscfg.tcbwtable),
            8UL);
-  memcpy((void *)(& ets->tc_tsa), (void const   *)(& dcbxcfg->etscfg.tsatable),
+  memmove((void *)(& ets->tc_tsa), (void const   *)(& dcbxcfg->etscfg.tsatable),
            8UL);
-  memcpy((void *)(& ets->prio_tc), (void const   *)(& dcbxcfg->etscfg.prioritytable),
+  memmove((void *)(& ets->prio_tc), (void const   *)(& dcbxcfg->etscfg.prioritytable),
            8UL);
-  memcpy((void *)(& ets->tc_reco_bw), (void const   *)(& dcbxcfg->etsrec.tcbwtable),
+  memmove((void *)(& ets->tc_reco_bw), (void const   *)(& dcbxcfg->etsrec.tcbwtable),
            8UL);
-  memcpy((void *)(& ets->tc_reco_tsa), (void const   *)(& dcbxcfg->etsrec.tsatable),
+  memmove((void *)(& ets->tc_reco_tsa), (void const   *)(& dcbxcfg->etsrec.tsatable),
            8UL);
-  memcpy((void *)(& ets->reco_prio_tc), (void const   *)(& dcbxcfg->etscfg.prioritytable),
+  memmove((void *)(& ets->reco_prio_tc), (void const   *)(& dcbxcfg->etscfg.prioritytable),
            8UL);
   return (0);
 }

c/ldv-linux-4.2-rc1/linux-4.2-rc1.tar.xz-08_1a-drivers--net--wireless--mwifiex--mwifiex.ko-entry_point_true-unreach-call.cil.out.c
@@ -12711,7 +12711,7 @@ static int mwifiex_set_mac_address(struct net_device *dev , void *addr )
   tmp = mwifiex_netdev_get_priv(dev);
   priv = tmp;
   hw_addr = (struct sockaddr *)addr;
-  memcpy((void *)(& priv->curr_addr), (void const   *)(& hw_addr->sa_data), 6UL);
+  memmove((void *)(& priv->curr_addr), (void const   *)(& hw_addr->sa_data), 6UL);
   ret = mwifiex_send_cmd(priv, 77, 1, 0U, (void *)0, 1);
   if (ret == 0) {
     memcpy((void *)(priv->netdev)->dev_addr, (void const   *)(& priv->curr_addr),
@@ -12727,7 +12727,7 @@ static int mwifiex_set_mac_address(struct net_device *dev , void *addr )
   } else {
 
   }
-  memcpy((void *)dev->dev_addr, (void const   *)(& priv->curr_addr), 6UL);
+  memmove((void *)dev->dev_addr, (void const   *)(& priv->curr_addr), 6UL);
   return (ret);
 }
 }

c/ldv-linux-4.2-rc1/linux-4.2-rc1.tar.xz-32_7a-drivers--media--usb--dvb-usb--dvb-usb-opera.ko-entry_point_true-unreach-call.cil.out.c
@@ -7255,7 +7255,7 @@ static int opera1_xilinx_rw(struct usb_device *dev , u8 request , u16 value , u8
   }
   if (flags == 1) {
     memcpy_guard((void *)buf, (void const   *)data, (size_t )len);
-    memcpy((void *)buf, (void const   *)data, (size_t )len);
+    memmove((void *)buf, (void const   *)data, (size_t )len);
   } else {
 
   }

c/ldv-linux-4.2-rc1/linux-4.2-rc1.tar.xz-32_7a-drivers--media--usb--dvb-usb-v2--dvb-usb-lmedm04.ko-entry_point_true-unreach-call.cil.out.c
@@ -7522,13 +7522,13 @@ static int lme2510_usb_talk(struct dvb_usb_device *d , u8 *wbuf , int wlen , u8 
   } else {
 
   }
-  memcpy((void *)buff, (void const   *)wbuf, (size_t )(64 < wlen ? 64 : wlen));
+  memmove((void *)buff, (void const   *)wbuf, (size_t )(64 < wlen ? 64 : wlen));
   tmp = lme2510_bulk_write(d->udev, buff, wlen, 1);
   ret = tmp | ret;
   tmp___0 = lme2510_bulk_read(d->udev, buff, 64 < rlen ? 64 : rlen, 1);
   ret = tmp___0 | ret;
   if (rlen > 0) {
-    memcpy((void *)rbuf, (void const   *)buff, (size_t )rlen);
+    memmove((void *)rbuf, (void const   *)buff, (size_t )rlen);
   } else {
 
   }

c/ldv-linux-4.2-rc1/linux-4.2-rc1.tar.xz-32_7a-drivers--net--ethernet--intel--i40e--i40e.ko-entry_point_true-unreach-call.cil.out.c
@@ -49650,19 +49650,19 @@ static int i40e_dcbnl_ieee_getets(struct net_device *dev , struct ieee_ets *ets 
   ets->willing = dcbxcfg->etscfg.willing;
   ets->ets_cap = dcbxcfg->etscfg.maxtcs;
   ets->cbs = dcbxcfg->etscfg.cbs;
-  memcpy((void *)(& ets->tc_tx_bw), (void const   *)(& dcbxcfg->etscfg.tcbwtable),
+  memmove((void *)(& ets->tc_tx_bw), (void const   *)(& dcbxcfg->etscfg.tcbwtable),
            8UL);
-  memcpy((void *)(& ets->tc_rx_bw), (void const   *)(& dcbxcfg->etscfg.tcbwtable),
+  memmove((void *)(& ets->tc_rx_bw), (void const   *)(& dcbxcfg->etscfg.tcbwtable),
            8UL);
-  memcpy((void *)(& ets->tc_tsa), (void const   *)(& dcbxcfg->etscfg.tsatable),
+  memmove((void *)(& ets->tc_tsa), (void const   *)(& dcbxcfg->etscfg.tsatable),
            8UL);
-  memcpy((void *)(& ets->prio_tc), (void const   *)(& dcbxcfg->etscfg.prioritytable),
+  memmove((void *)(& ets->prio_tc), (void const   *)(& dcbxcfg->etscfg.prioritytable),
            8UL);
-  memcpy((void *)(& ets->tc_reco_bw), (void const   *)(& dcbxcfg->etsrec.tcbwtable),
+  memmove((void *)(& ets->tc_reco_bw), (void const   *)(& dcbxcfg->etsrec.tcbwtable),
            8UL);
-  memcpy((void *)(& ets->tc_reco_tsa), (void const   *)(& dcbxcfg->etsrec.tsatable),
+  memmove((void *)(& ets->tc_reco_tsa), (void const   *)(& dcbxcfg->etsrec.tsatable),
            8UL);
-  memcpy((void *)(& ets->reco_prio_tc), (void const   *)(& dcbxcfg->etscfg.prioritytable),
+  memmove((void *)(& ets->reco_prio_tc), (void const   *)(& dcbxcfg->etscfg.prioritytable),
            8UL);
   return (0);
 }

c/ldv-linux-4.2-rc1/linux-4.2-rc1.tar.xz-32_7a-drivers--net--wireless--mwifiex--mwifiex.ko-entry_point_true-unreach-call.cil.out.c
@@ -12721,7 +12721,7 @@ static int mwifiex_set_mac_address(struct net_device *dev , void *addr )
   tmp = mwifiex_netdev_get_priv(dev);
   priv = tmp;
   hw_addr = (struct sockaddr *)addr;
-  memcpy((void *)(& priv->curr_addr), (void const   *)(& hw_addr->sa_data), 6UL);
+  memmove((void *)(& priv->curr_addr), (void const   *)(& hw_addr->sa_data), 6UL);
   ret = mwifiex_send_cmd(priv, 77, 1, 0U, (void *)0, 1);
   if (ret == 0) {
     memcpy((void *)(priv->netdev)->dev_addr, (void const   *)(& priv->curr_addr),
@@ -12737,7 +12737,7 @@ static int mwifiex_set_mac_address(struct net_device *dev , void *addr )
   } else {
 
   }
-  memcpy((void *)dev->dev_addr, (void const   *)(& priv->curr_addr), 6UL);
+  memmove((void *)dev->dev_addr, (void const   *)(& priv->curr_addr), 6UL);
   return (ret);
 }
 }

c/ldv-linux-4.2-rc1/linux-4.2-rc1.tar.xz-43_2a-drivers--bluetooth--hci_uart.ko-entry_point_true-unreach-call.cil.out.c
@@ -9050,7 +9050,7 @@ static int h4_enqueue(struct hci_uart *hu , struct sk_buff *skb )
 
   }
   tmp___0 = skb_push(skb, 1U);
-  memcpy((void *)tmp___0, (void const   *)(& ((struct bt_skb_cb *)(& skb->cb))->pkt_type),
+  memmove((void *)tmp___0, (void const   *)(& ((struct bt_skb_cb *)(& skb->cb))->pkt_type),
            1UL);
   skb_queue_tail(& h4->txq, skb);
   return (0);
@@ -12677,7 +12677,7 @@ static int ath_enqueue(struct hci_uart *hu , struct sk_buff *skb )
 
   }
   tmp___0 = skb_push(skb, 1U);
-  memcpy((void *)tmp___0, (void const   *)(& ((struct bt_skb_cb *)(& skb->cb))->pkt_type),
+  memmove((void *)tmp___0, (void const   *)(& ((struct bt_skb_cb *)(& skb->cb))->pkt_type),
            1UL);
   skb_queue_tail(& ath->txq, skb);
   set_bit(1L, (unsigned long volatile   *)(& hu->tx_state));
@@ -14239,7 +14239,7 @@ static void h5_slip_delim(struct sk_buff *skb )
   {
   delim = -64;
   tmp = skb_put(skb, 1U);
-  memcpy((void *)tmp, (void const   *)(& delim), 1UL);
+  memmove((void *)tmp, (void const   *)(& delim), 1UL);
   return;
 }
 }
@@ -14259,7 +14259,7 @@ static void h5_slip_one_byte(struct sk_buff *skb , u8 c )
   switch ((int )c) {
   case 192: 
   tmp = skb_put(skb, 2U);
-  memcpy((void *)tmp, (void const   *)(& esc_delim), 2UL);
+  memmove((void *)tmp, (void const   *)(& esc_delim), 2UL);
   goto ldv_50458;
   case 219: 
   tmp___0 = skb_put(skb, 2U);
@@ -14267,7 +14267,7 @@ static void h5_slip_one_byte(struct sk_buff *skb , u8 c )
   goto ldv_50458;
   default: 
   tmp___1 = skb_put(skb, 1U);
-  memcpy((void *)tmp___1, (void const   *)(& c), 1UL);
+  memmove((void *)tmp___1, (void const   *)(& c), 1UL);
   }
   ldv_50458: ;
   return;
@@ -15549,7 +15549,7 @@ static int bcm_enqueue(struct hci_uart *hu , struct sk_buff *skb )
 
   }
   tmp___0 = skb_push(skb, 1U);
-  memcpy((void *)tmp___0, (void const   *)(& ((struct bt_skb_cb *)(& skb->cb))->pkt_type),
+  memmove((void *)tmp___0, (void const   *)(& ((struct bt_skb_cb *)(& skb->cb))->pkt_type),
            1UL);
   skb_queue_tail(& bcm->txq, skb);
   return (0);

commit 1280ba1fd2173bf8b80cfa63735ec20c08f46f65

Add memmove prototypes to LDV benchmarks
This fixes a problem introduced in 55d4083, where some calls to memcpy
were converted to calls to memmove, but the files did not have a
prototype for memmove.

c/ldv-linux-4.2-rc1/linux-4.2-rc1.tar.xz-08_1a-drivers--media--usb--dvb-usb--dvb-usb-dw2102.ko-entry_point_false-unreach-call.cil.out.c
@@ -7165,6 +7165,7 @@ typedef int ldv_func_ret_type;
 extern struct module __this_module ;
 extern int printk(char const   *  , ...) ;
 extern void *memcpy(void * , void const   * , size_t  ) ;
+extern void *memmove(void * , void const   * , size_t  ) ;
 extern void *memset(void * , int  , size_t  ) ;
 extern size_t strlcpy(char * , char const   * , size_t  ) ;
 extern void *kmemdup(void const   * , size_t  , gfp_t  ) ;

c/ldv-linux-4.2-rc1/linux-4.2-rc1.tar.xz-08_1a-drivers--net--ethernet--intel--i40e--i40e.ko-entry_point_true-unreach-call.cil.out.c
@@ -9407,6 +9407,7 @@ extern void __bad_percpu_size(void) ;
 extern void __bad_size_call_parameter(void) ;
 extern void warn_slowpath_null(char const   * , int const    ) ;
 extern void *memcpy(void * , void const   * , size_t  ) ;
+extern void *memmove(void * , void const   * , size_t  ) ;
 extern void *memset(void * , int  , size_t  ) ;
 extern int memcmp(void const   * , void const   * , size_t  ) ;
 extern char *strncpy(char * , char const   * , __kernel_size_t  ) ;

c/ldv-linux-4.2-rc1/linux-4.2-rc1.tar.xz-08_1a-drivers--net--wireless--mwifiex--mwifiex.ko-entry_point_true-unreach-call.cil.out.c
@@ -10776,6 +10776,7 @@ __inline static int list_empty(struct list_head  const  *head )
 }
 }
 extern void *memcpy(void * , void const   * , size_t  ) ;
+extern void *memmove(void * , void const   * , size_t  ) ;
 extern void *memset(void * , int  , size_t  ) ;
 extern void *__memmove(void * , void const   * , size_t  ) ;
 extern size_t strlen(char const   * ) ;

c/ldv-linux-4.2-rc1/linux-4.2-rc1.tar.xz-32_7a-drivers--media--usb--dvb-usb--dvb-usb-opera.ko-entry_point_true-unreach-call.cil.out.c
@@ -6990,6 +6990,7 @@ typedef int ldv_func_ret_type___2;
 extern struct module __this_module ;
 extern int printk(char const   *  , ...) ;
 extern void *memcpy(void * , void const   * , size_t  ) ;
+extern void *memmove(void * , void const   * , size_t  ) ;
 extern void *memset(void * , int  , size_t  ) ;
 extern int mutex_trylock(struct mutex * ) ;
 int ldv_mutex_trylock_8(struct mutex *ldv_func_arg1 ) ;

c/ldv-linux-4.2-rc1/linux-4.2-rc1.tar.xz-32_7a-drivers--media--usb--dvb-usb-v2--dvb-usb-lmedm04.ko-entry_point_true-unreach-call.cil.out.c
@@ -7028,6 +7028,7 @@ extern struct module __this_module ;
 extern int printk(char const   *  , ...) ;
 extern void __dynamic_pr_debug(struct _ddebug * , char const   *  , ...) ;
 extern void *memcpy(void * , void const   * , size_t  ) ;
+extern void *memmove(void * , void const   * , size_t  ) ;
 extern void *memset(void * , int  , size_t  ) ;
 extern size_t strlcpy(char * , char const   * , size_t  ) ;
 extern size_t strlcat(char * , char const   * , __kernel_size_t  ) ;

c/ldv-linux-4.2-rc1/linux-4.2-rc1.tar.xz-32_7a-drivers--net--ethernet--intel--i40e--i40e.ko-entry_point_true-unreach-call.cil.out.c
@@ -9396,6 +9396,7 @@ __inline static void hlist_del(struct hlist_node *n )
 }
 }
 extern void *memcpy(void * , void const   * , size_t  ) ;
+extern void *memmove(void * , void const   * , size_t  ) ;
 extern void *memset(void * , int  , size_t  ) ;
 extern int memcmp(void const   * , void const   * , size_t  ) ;
 extern char *strncpy(char * , char const   * , __kernel_size_t  ) ;

c/ldv-linux-4.2-rc1/linux-4.2-rc1.tar.xz-32_7a-drivers--net--wireless--mwifiex--mwifiex.ko-entry_point_true-unreach-call.cil.out.c
@@ -10769,6 +10769,7 @@ __inline static int list_empty(struct list_head  const  *head )
 }
 }
 extern void *memcpy(void * , void const   * , size_t  ) ;
+extern void *memmove(void * , void const   * , size_t  ) ;
 extern void *memset(void * , int  , size_t  ) ;
 extern void *__memmove(void * , void const   * , size_t  ) ;
 extern size_t strlen(char const   * ) ;

c/ldv-linux-4.2-rc1/linux-4.2-rc1.tar.xz-43_2a-drivers--bluetooth--hci_uart.ko-entry_point_true-unreach-call.cil.out.c
@@ -8828,6 +8828,7 @@ __inline static __u16 __le16_to_cpup(__le16 const   *p )
 }
 void *ldv_err_ptr(long error ) ;
 extern void *memcpy(void * , void const   * , size_t  ) ;
+extern void *memmove(void * , void const   * , size_t  ) ;
 __inline static void *ERR_PTR(long error ) ;
 __inline static long PTR_ERR(void const   *ptr ) ;
 __inline static bool IS_ERR(void const   *ptr ) ;

c/ldv-linux-4.2-rc1/linux-4.2-rc1.tar.xz-43_2a-drivers--net--ethernet--amd--xgbe--amd-xgbe.ko-entry_point_true-unreach-call.cil.out.c
@@ -7026,6 +7026,7 @@ __inline static long ldv__builtin_expect(long exp , long c )
 }
 }
 extern void *memcpy(void * , void const   * , size_t  ) ;
+extern void *memmove(void * , void const   * , size_t  ) ;
 extern int strcmp(char const   * , char const   * ) ;
 extern char *strchr(char const   * , int  ) ;
 extern int __bitmap_weight(unsigned long const   * , unsigned int  ) ;

c/ldv-linux-4.2-rc1/linux-4.2-rc1.tar.xz-43_2a-drivers--net--ethernet--natsemi--natsemi.ko-entry_point_true-unreach-call.cil.out.c
@@ -5707,6 +5707,7 @@ __inline static long ldv__builtin_expect(long exp , long c )
 }
 extern unsigned long __phys_addr(unsigned long  ) ;
 extern void *memcpy(void * , void const   * , size_t  ) ;
+extern void *memmove(void * , void const   * , size_t  ) ;
 extern void *memset(void * , int  , size_t  ) ;
 extern size_t strlcpy(char * , char const   * , size_t  ) ;
 extern int strncmp(char const   * , char const   * , __kernel_size_t  ) ;

c/ldv-linux-4.2-rc1/linux-4.2-rc1.tar.xz-43_2a-drivers--net--fddi--defxx.ko-entry_point_true-unreach-call.cil.out.c
@@ -6369,6 +6369,7 @@ __inline static long ldv__builtin_expect(long exp , long c )
 }
 extern unsigned long __phys_addr(unsigned long  ) ;
 extern void *memcpy(void * , void const   * , size_t  ) ;
+extern void *memmove(void * , void const   * , size_t  ) ;
 extern void *memset(void * , int  , size_t  ) ;
 extern void __raw_spin_lock_init(raw_spinlock_t * , char const   * , struct lock_class_key * ) ;
 extern void _raw_spin_lock(raw_spinlock_t * ) ;

c/ldv-linux-4.2-rc1/linux-4.2-rc1.tar.xz-43_2a-drivers--scsi--bnx2i--bnx2i.ko-entry_point_true-unreach-call.cil.out.c
@@ -8354,6 +8354,7 @@ __inline static int list_empty(struct list_head  const  *head )
 extern unsigned long __per_cpu_offset[8192U] ;
 extern void warn_slowpath_null(char const   * , int const    ) ;
 extern void *memcpy(void * , void const   * , size_t  ) ;
+extern void *memmove(void * , void const   * , size_t  ) ;
 extern size_t strlcpy(char * , char const   * , size_t  ) ;
 extern int nr_cpu_ids ;
 extern struct cpumask  const  * const  cpu_possible_mask ;

c/ldv-linux-4.2-rc1/linux-4.2-rc1.tar.xz-43_2a-drivers--tty--synclink_gt.ko-entry_point_true-unreach-call.cil.out.c
@@ -6177,6 +6177,7 @@ __inline static struct task_struct *get_current(void)
 }
 }
 extern void *memcpy(void * , void const   * , size_t  ) ;
+extern void *memmove(void * , void const   * , size_t  ) ;
 extern void *memset(void * , int  , size_t  ) ;
 extern int memcmp(void const   * , void const   * , size_t  ) ;
 extern char *strcat(char * , char const   * ) ;