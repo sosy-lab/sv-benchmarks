sosy-lab/sv-benchmarks

commit f9eb01ead14c5d2904c86c27cf264f4ce4653e54

Use Java Verifier class in jbmc-regression

java/jbmc-regression/cast1/Main.java
@@ -6,12 +6,14 @@
  *     directory: regression/cbmc-java/cast1
  * The benchmark was taken from the repo: 24 January 2018
  */
+import org.sosy_lab.sv_benchmarks.Verifier;
+
 class Main
 {
   public static void main(String[] args)
   {
-    int i = args.length;
-    if(args.length<-128 || args.length>127)
+    int i = Verifier.nondetInt();
+    if(i < -128 || i > 127)
       return;
     byte b = (byte) i;
     assert b == i;

java/jbmc-regression/ArithmeticException1/Main.java
@@ -6,10 +6,12 @@
  *     directory: regression/cbmc-java/ArithmeticException1
  * The benchmark was taken from the repo: 24 January 2018
  */
+import org.sosy_lab.sv_benchmarks.Verifier;
+
 public class Main {
     public static void  main(String args[]) {
         try {
-            int i=args.length;
+            int i = Verifier.nondetInt();
             int j=10/i;
         }
         catch(ArithmeticException exc) {

java/jbmc-regression/ArithmeticException6/Main.java
@@ -6,9 +6,11 @@
  *     directory: regression/cbmc-java/ArithmeticException6
  * The benchmark was taken from the repo: 24 January 2018
  */
+import org.sosy_lab.sv_benchmarks.Verifier;
+
 public class Main {
     public static void  main(String[] args) {
-        int denom = args.length - 5;
+        int denom = Verifier.nondetInt();
         try {
             int j=10/denom;
         }

java/jbmc-regression/ArrayIndexOutOfBoundsException1/Main.java
@@ -6,11 +6,16 @@
  *     directory: regression/cbmc-java/ArrayIndexOutOfBoundsException1
  * The benchmark was taken from the repo: 24 January 2018
  */
+import org.sosy_lab.sv_benchmarks.Verifier;
+
 public class Main {
   public static void main(String args[]) {
+      int size = Verifier.nondetInt();
+      if (size < 0)
+        return;
       try {
           int[] a=new int[4];
-          a[args.length]=0;
+          a[size]=0;
       }
       catch (ArrayIndexOutOfBoundsException exc) {
           assert false;

java/jbmc-regression/ArrayIndexOutOfBoundsException2/Main.java
@@ -6,11 +6,16 @@
  *     directory: regression/cbmc-java/ArrayIndexOutOfBoundsException2
  * The benchmark was taken from the repo: 24 January 2018
  */
+import org.sosy_lab.sv_benchmarks.Verifier;
+
 public class Main {
   public static void main(String args[]) {
+      int size = Verifier.nondetInt();
+      if (size < 0)
+        return;
       try {
           int[] a=new int[4];
-          int i=a[args.length];
+          int i=a[size];
       }
       catch (ArrayIndexOutOfBoundsException exc) {
           assert false;

java/jbmc-regression/ArrayIndexOutOfBoundsException3/Main.java
@@ -6,11 +6,13 @@
  *     directory: regression/cbmc-java/ArrayIndexOutOfBoundsException3
  * The benchmark was taken from the repo: 24 January 2018
  */
+import org.sosy_lab.sv_benchmarks.Verifier;
+
 public class Main {
   public static void main(String args[]) {
       try {
           int[] a=new int[4];
-          a[args.length]=0;
+          a[Verifier.nondetInt()]=0;
       }
       catch (Exception exc) {
           assert false;

java/jbmc-regression/BufferedReaderReadLine/Main.java
@@ -7,7 +7,8 @@
  * The benchmark was taken from the repo: 24 January 2018
  */
 import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.StringReader;
+import org.sosy_lab.sv_benchmarks.Verifier;
 
 public class Main {
 
@@ -17,11 +18,12 @@ public class Main {
         }
 
     public static void main(String[] args) {
+        String arg = Verifier.nondetString();
         String thisLine = null;
         int numLines = 0;
 
         try {
-            BufferedReader br = new BufferedReader(new FileReader(args[1]));
+            BufferedReader br = new BufferedReader(new StringReader(arg));
             String line = check(br);
             while ((thisLine = check(br)) != null) {
                 System.out.println(thisLine);

java/jbmc-regression/CharSequenceBug/Main.java
@@ -6,11 +6,11 @@
  *     directory: regression/jbmc-strings-test-gen/CharSequenceBug
  * The benchmark was taken from the repo: 24 January 2018
  */
+import org.sosy_lab.sv_benchmarks.Verifier;
+
 public class Main {
     public static void main(String[] args) {
-        if (args.length < 1)
-            return;
-        String s = args[0];
+        String s = Verifier.nondetString();
         CharSequence target = "b";
         String replaced = "";
         if (target.length() == 1)

commit 402502e218c28a464d0d8c4f51320b399b2119f6

Fix bug introduced in f9eb01e
Originally i was positive, which was probably not intended
as the range assumption indicates.
Since char is unsigned the numerical value will not
match the value of i for negative numbers, the lower
16 bits, however, will be identical.

java/jbmc-regression/cast1/Main.java
@@ -18,7 +18,7 @@ class Main {
     short s = (short)i;
     assert s == i;
     char c = (char)i;
-    assert c == i;
+    assert c == (0xffff & i);
     long l = (long)i;
     assert l == i;
     float f = (float)i;