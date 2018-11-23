91ffeec77158069d3e787ce1dde199c62308bdd0
Marek Chalupa <xchalup4@fi.muni.cz>
2018-11-23 12:32:21 +0100

    busybox: add missing chroot header file
    Forgot to 'git add' it.

---
+++ c/busybox-1.22.0/busybox_sv_comp-chroot.h
@@ -0,0 +1,10 @@
+int chroot(const char *path)
+{
+    (void)*path;
+    if (__VERIFIER_nondet_int()) {
+        *bb_errno = __VERIFIER_nondet_int();
+        __VERIFIER_assume(*bb_errno != 0);
+        return -1;
+    }
+    return 0;
+}
