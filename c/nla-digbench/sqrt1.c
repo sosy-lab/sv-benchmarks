0b32360de184e2974fffff0f5c5f341569604358
ThanhVu (Vu) Nguyen <nguyenthanhvuh@gmail.com>
2019-03-31 23:49:38 -0500

    add sqrt1.c

--- c/nla-digbench/sqrt1.c
+++ c/nla-digbench/sqrt1.c
@@ -1,38 +1,34 @@
-#include <stdio.h>
-#include <assert.h>
-#include <stdlib.h>
+/* Compute the floor of the square root of a natural number */
 
+extern void __VERIFIER_error() __attribute__ ((__noreturn__));
+extern int __VERIFIER_nondet_int(void);
+extern void __VERIFIER_assume(int expression);
+void __VERIFIER_assert(int cond) { if (!(cond)) { ERROR: __VERIFIER_error(); } return; }
 
-//square root of a natural number
+#include <math.h>
 
-int sqrt1(int n){
-  assert(n >= 0);
-  
-  int a,s,t;
-  a=0;
-  s=1;
-  t=1;
+int main(){
+    int n, a, s, t;
+    n = __VERIFIER_nondet_int();
+    __VERIFIER_assume(n >= 0);
 
-  int ctr = 0;
-  while(1){
-    //assert(a*a <= n);
-    //assert(t == 2*a + 1);
-    //assert(s == (a + 1)*(a + 1));
-    //the above 2 should be equiv to t^2 - 4*s + 2*t + 1 == 0
-	  
-    if(!(s <= n)) break;
-    a=a+1;
-    t=t+2;
-    s=s+t;
-  }
+    a = 0;
+    s = 1;
+    t = 1;
 
-  return a;
-     
+    while(1){
+        __VERIFIER_assert(a*a <= n);
+        __VERIFIER_assert(t == 2*a + 1);
+        __VERIFIER_assert(s == (a + 1)*(a + 1));
+        // the above 2 should be equiv to t^2 - 4*s + 2*t + 1 == 0
+
+        if(!(s <= n)) 
+            break;
+
+        a = a + 1;
+        t = t + 2;
+        s = s + t;
+    }
+    __VERIFIER_assert(a == (int)floor(sqrt(n)));
+    return 0;
 }
-
-
-int main(int argc, char **argv){
-  sqrt1(atoi(argv[1]));
-  return 0;
-}
-
