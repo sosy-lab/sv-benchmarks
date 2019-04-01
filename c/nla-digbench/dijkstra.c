f16636fc6564f0e18a731fde3538f024d95e6246
ThanhVu (Vu) Nguyen <nguyenthanhvuh@gmail.com>
2019-03-31 23:25:01 -0500

    add post for Dijkstra

--- c/nla-digbench/dijkstra.c
+++ c/nla-digbench/dijkstra.c
@@ -1,58 +1,57 @@
-/*
-  a square root algorithm
-*/
+/* Compute the floor of the square root, by Dijkstra */
 
 extern void __VERIFIER_error() __attribute__ ((__noreturn__));
 extern int __VERIFIER_nondet_int(void);
 extern void __VERIFIER_assume(int expression);
 void __VERIFIER_assert(int cond) { if (!(cond)) { ERROR: __VERIFIER_error(); } return; }
 
+#include <math.h>
+
 int main(){
-  int n, p, q , r, h;
+    int n, p, q , r, h;
 
-  n = __VERIFIER_nondet_int();
-  __VERIFIER_assume(n >= 0);
+    n = __VERIFIER_nondet_int();
+    __VERIFIER_assume(n >= 0);
 
-  p = 0;
-  q = 1;
-  r = n;
-  h = 0;
-  while (1){
-    if(!(q <= n))
-      break;
+    p = 0;
+    q = 1;
+    r = n;
+    h = 0;
+    while (1){
+        if(!(q <= n))
+            break;
 
-    q = 4 * q;
-    __VERIFIER_assert(n >= 0);
-    __VERIFIER_assert(p == 0);
-    __VERIFIER_assert(r==n);
-  }
-  //q = 4^n
-
-  while (1){
-    __VERIFIER_assert(r < 2 * p + q);
-    __VERIFIER_assert(p * p + r * q == n * q);
-    __VERIFIER_assert(h*h*h - 12*h*n*q + 16*n*p*q - h*q*q
-                      - 4*p*q*q + 12*h*q*r - 16*p*q*r == 0);
-    __VERIFIER_assert(h*h*n - 4*h*n*p + 4*(n*n)*q - n*q*q
-                      - h*h*r + 4*h*p*r - 8*n*q*r + q*q*r + 4*q*r*r == 0);
-    __VERIFIER_assert(h*h*p - 4*h*n*q + 4*n*p*q - p*q*q
-                      + 4*h*q*r - 4*p*q*r == 0);
-    __VERIFIER_assert(p*p - n*q + q*r == 0);
-
-    if(!(q != 1))
-      break;
-
-    q = q / 4;
-    h = p + q;
-    p = p / 2;
-    if (r >= h){
-      p = p + q;
-      r = r -h;
+        q = 4 * q;
+        __VERIFIER_assert(n >= 0);
+        __VERIFIER_assert(p == 0);
+        __VERIFIER_assert(r == n);
     }
-  }
+    //q == 4^n
 
+    while (1){
+        __VERIFIER_assert(r < 2 * p + q);
+        __VERIFIER_assert(p * p + r * q == n * q);
+        __VERIFIER_assert(h*h*h - 12*h*n*q + 16*n*p*q - h*q*q
+                - 4*p*q*q + 12*h*q*r - 16*p*q*r == 0);
+        __VERIFIER_assert(h*h*n - 4*h*n*p + 4*(n*n)*q - n*q*q
+                - h*h*r + 4*h*p*r - 8*n*q*r + q*q*r + 4*q*r*r == 0);
+        __VERIFIER_assert(h*h*p - 4*h*n*q + 4*n*p*q - p*q*q
+                + 4*h*q*r - 4*p*q*r == 0);
+        __VERIFIER_assert(p*p - n*q + q*r == 0);
 
-  //tvn: missing post
-  return 0;
+        if(!(q != 1))
+            break;
+
+        q = q / 4;
+        h = p + q;
+        p = p / 2;
+        if (r >= h){
+            p = p + q;
+            r = r -h;
+        }
+    }
+
+    __VERIFIER_assert(p == (int)floor(sqrt(n)));
+    return 0;
 }
 
