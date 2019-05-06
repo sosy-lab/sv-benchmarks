c7b5dc2da680aa1aa09481639c8827e63867c046
mbuhl <git@moritzbuhl.de>
2019-05-06 20:22:09 +0200

    Add exceptions for compare.sh

--- c/compare.sh
+++ c/compare.sh
@@ -60,7 +60,7 @@ for f in $SETS ; do
   if [ $setf = ConcurrencySafety-Main ] ; then
     echo "Skipping category $setf (platform-dependent types)"
     continue
-  elif [[ $SKIP_LARGE -eq 1 && $setf == Systems_DeviceDriversLinux64_ReachSafety* ]] ; then
+  elif [[ $SKIP_LARGE -eq 1 && ( $setf == Systems_DeviceDriversLinux64_ReachSafety* || $setf == "Systems_OpenBSD_MemSafety" ) ]]; then
     echo "Skipping category $setf (only custom includes, no system headers, checking takes too much time)"
     continue
   fi
