35d43ba9a8cb387fbdd962d3179aced5aace2c27
mbuhl <git@moritzbuhl.de>
2019-05-13 21:23:38 +0200

    Adjust styling of my changes

--- c/compare.sh
+++ c/compare.sh
@@ -60,10 +60,10 @@ for f in $SETS ; do
   if [ $setf = ConcurrencySafety-Main ] ; then
     echo "Skipping category $setf (platform-dependent types)"
     continue
-  elif [[ $SKIP_LARGE -eq 1 && $setf == Systems_DeviceDriversLinux64_ReachSafety* ]]; then
+  elif [[ $SKIP_LARGE -eq 1 && $setf == Systems_DeviceDriversLinux64_ReachSafety* ]] ; then
     echo "Skipping category $setf (only custom includes, no system headers, checking takes too much time)"
     continue
-  elif [[ $setf == Systems_OpenBSD_MemSafety ]]; then
+  elif [ $setf = Systems_OpenBSD_MemSafety ] ; then
     echo "Skipping category $setf (only custom includes, no system headers, complicated build process)"
     continue
   fi
