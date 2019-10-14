b2569e80525d97860169abcf2a87188962aa8333
Gidon Ernst <gidon.ernst@sosy.ifi.lmu.de>
2019-10-14 10:48:37 +0200

    don't refer to intermediate c files

--- c/compare.sh
+++ c/compare.sh
@@ -113,6 +113,8 @@ for f in $SETS ; do
     # no original source available
     if echo $ff | grep -q '^ddv-machzwd/' ; then
       continue
+    if echo $ff | grep -q '^aws-c-common/' ; then
+      continue
     elif [ $ff = "loops/s3.i" ] ; then
       continue
     elif echo $ff | egrep -q '^ldv-(linux-3.0|regression)/' ; then
