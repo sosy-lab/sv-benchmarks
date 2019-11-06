8ff24d347f6be6a1fa33032407b9a2db34dd3870
Michael Tautschnig <tautschn@amazon.com>
2019-11-06 20:25:30 +0000

    seq-pthread: Replace all uses of nondet_pointer by nondet_ulong
    The semantics of __VERIFIER_nondet_pointer, particular whether it does
    or does not perform memory allocation, have never been clarified (see
    issue #767). To avoid any such ambiguity, just cast a non-deterministic
    unsigned long to a void* instead, making clear that no memory allocation
    is performed. Indeed, these benchmarks just perform equality tests on
    the pointer values, but never attempt to deference them.
    
    The changes were applied as follows:
    ```
    perl -p -i -e \
      's/extern void \* ?__VERIFIER_nondet_pointer\(void\);/extern unsigned long __VERIFIER_nondet_ulong(void);/' \
      c/seq-pthread/*
    perl -p -i -e \
      's/__VERIFIER_nondet_pointer\(\);/(void *)__VERIFIER_nondet_ulong();/' c/seq-pthread/*
    ```

--- c/seq-pthread/cs_sync.c
+++ c/seq-pthread/cs_sync.c
@@ -1,5 +1,5 @@
 extern int __VERIFIER_nondet_int(void);
-extern void * __VERIFIER_nondet_pointer(void);
+extern unsigned long __VERIFIER_nondet_ulong(void);
 extern unsigned char __VERIFIER_nondet_uchar(void);
 extern void __VERIFIER_assume(int);
 extern void __VERIFIER_error() __attribute__ ((__noreturn__));
@@ -409,7 +409,7 @@ int main()
 	  __CS_cp_full[i] = __VERIFIER_nondet_uchar();
 	  for(j = 0; j < 3; j++) {
 	    __CS_cp___CS_thread_status[i][j] = __VERIFIER_nondet_uchar();
-	    __CS_cp___CS_thread_lockedon[i][j] = (unsigned char *) __VERIFIER_nondet_pointer();
+	    __CS_cp___CS_thread_lockedon[i][j] = (unsigned char *) (void *)__VERIFIER_nondet_ulong();
 	  }
 	}
 	//cseq: Copy statements for global variables:
