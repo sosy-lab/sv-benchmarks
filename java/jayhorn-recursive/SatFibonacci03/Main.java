bb0ce178038f91172ed374966981aea75e295f94
Peter Schrammel <peter.schrammel@diffblue.com>
2019-11-25 21:31:30 +0000

    Fix invalid main methods
    Non-public main methods don't comply with the rules
    and cannot be executed with `java`.

--- java/jayhorn-recursive/SatFibonacci03/Main.java
+++ java/jayhorn-recursive/SatFibonacci03/Main.java
@@ -20,7 +20,7 @@ public class Main {
     }
   }
 
-  static void main(String[] args) {
+  public static void main(String[] args) {
     int x = Verifier.nondetInt();
     if (x > 46) {
       return;
