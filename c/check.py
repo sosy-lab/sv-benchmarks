896d5f141bf41b1bee27f15846499e0750b8a551
feliperodri <rms.felipe@gmail.com>
2019-11-25 11:43:54 -0400

    Apply s_remove_node fix when rebuilding benchmarks
    Signed-off-by: feliperodri <rms.felipe@gmail.com>

--- c/check.py
+++ c/check.py
@@ -103,6 +103,7 @@ KNOWN_DIRECTORY_PROBLEMS = [
 
     ("aws-c-common", "unexpected file patch.diff"),
     ("aws-c-common", "unexpected file s_swap-fix.diff"),
+    ("aws-c-common", "unexpected file s_remove_node-fix.diff"),
     ("aws-c-common", "unexpected file overflow-fix-1.diff"),
     ("aws-c-common", "unexpected file overflow-fix-2.diff"),
     ("aws-c-common", "unexpected file makeall"),
