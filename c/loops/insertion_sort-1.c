f424111b486a5b853d4777abf2ad08808cc1505b
Rafael Sá Menezes <rafael.sa@icomp.ufam.edu.br>
2020-01-10 17:35:42 -0400

    initialized nondeterministic array
    Signed-off-by: Rafael Sá Menezes <rafael.sa@icomp.ufam.edu.br>

--- c/loops/insertion_sort-1.c
+++ c/loops/insertion_sort-1.c
@@ -10,7 +10,9 @@ unsigned int __VERIFIER_nondet_uint();
 int main() {
    unsigned int SIZE=__VERIFIER_nondet_uint();
    int i, j, k, key;
-   int v[SIZE];   
+   int v[SIZE];
+   for (j=0;j<SIZE;j++)
+      v[j] = __VERIFIER_nondet_int();  
    for (j=1;j<SIZE;j++) {	  
       key = v[j];
       i = j - 1;
--- c/loops/insertion_sort-2.c
+++ c/loops/insertion_sort-2.c
@@ -11,6 +11,8 @@ int main() {
    unsigned int SIZE=__VERIFIER_nondet_uint();
    int i, j, k, key;
    int v[SIZE];   
+   for (j=0;j<SIZE;j++)
+      v[j] = __VERIFIER_nondet_int();
    for (j=1;j<SIZE;j++) {	  
       key = v[j];
       i = j - 1;
