57a197db98db32e919dd803f01068be3217afb5e
Karlheinz Friedberger <kfriedberger@gmail.com>
2020-09-29 21:06:45 +0200

    adding tasks with nested loops.
    Those tasks were taken from CPAchecker's repository.

---
+++ c/loop-simple/nested1b.c
@@ -0,0 +1,23 @@
+// This file is part of the SV-Benchmarks collection of verification tasks:
+// https://github.com/sosy-lab/sv-benchmarks
+//
+// This file was part of CPAchecker,
+// a tool for configurable software verification:
+// https://cpachecker.sosy-lab.org
+//
+// SPDX-FileCopyrightText: 2007-2020 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+int main() {
+	int a = 6;
+
+
+	for(a = 0; a < 6; ++a) {
+
+	}
+	if(a == 6 ) {
+		ERROR: goto ERROR;
+	}
+	return 1;
+}
