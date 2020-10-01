ccec1d9366e4de32efe0ac09358230046f98b4b9
Karlheinz Friedberger <kfriedberger@gmail.com>
2020-10-01 13:00:05 +0200

    insert body into reach_error calling assert_fail.

--- c/loop-simple/nested_2.c
+++ c/loop-simple/nested_2.c
@@ -9,7 +9,8 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-void reach_error(){}
+extern void __assert_fail(const char *, const char *, unsigned int, const char *) __attribute__ ((__nothrow__ , __leaf__)) __attribute__ ((__noreturn__));
+void reach_error() { __assert_fail("0", "nested_2.c", 13, "reach_error"); }
 
 int main() {
 	int a = 6;
