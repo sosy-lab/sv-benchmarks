4203002245b07a2a6670e7d466c9db2f4062cabb
Karlheinz Friedberger <kfriedberger@gmail.com>
2020-10-01 13:01:40 +0200

    insert body into reach_error calling assert_fail.

--- c/loop-simple/deep-nested.c
+++ c/loop-simple/deep-nested.c
@@ -1,5 +1,5 @@
-extern void abort(void);
-void reach_error(){}
+extern void __assert_fail(const char *, const char *, unsigned int, const char *) __attribute__ ((__nothrow__ , __leaf__)) __attribute__ ((__noreturn__));
+void reach_error() { __assert_fail("0", "deep-nested.c", 2, "reach_error"); }
 
 int main() {
 	unsigned a, b, c, d, e;
