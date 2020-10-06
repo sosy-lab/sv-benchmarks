3506183995b27cbbcd43dba85c4c24eee5eb2672
Martin Spiessl <git@2t7.de>
2020-10-06 14:02:33 +0200

    Include assert.h in .c files, add .i files

--- c/loop-zilu/benchmark39_conjunctive.c
+++ c/loop-zilu/benchmark39_conjunctive.c
@@ -1,10 +1,5 @@
-extern void __assert_fail (__const char *__assertion, __const char *__file,
-      unsigned int __line, __const char *__function)
-     __attribute__ ((__nothrow__ , __leaf__)) __attribute__ ((__noreturn__));
-extern int __VERIFIER_nondet_int(void);
-extern _Bool __VERIFIER_nondet_bool(void);
-
-void reach_error(void) {__assert_fail ("0", "benchmark39_conjunctive.c", 11, __PRETTY_FUNCTION__);}
+#include <assert.h>
+void reach_error(void) {assert(0);}
 
 void __VERIFIER_assert(int cond) {
   if (!cond) {
